dd459229837d245b41e4342ce03aa030bb41892c
ceren kotan <cerenkotann@gmail.com>
2026-06-04 12:45:59 +0300

    düzeltmeler2

--- 12 Burç Oyunu/Assets/Kodlar/Player.move.cs
+++ 12 Burç Oyunu/Assets/Kodlar/Player.move.cs
@@ -1,33 +1,34 @@
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMove : MonoBehaviour // Not: E�er Unity hata verirse script dosyas�n�n ad�n� da PlayerMove.cs olarak de�i�tirmen gerekebilir.
 {
-    public float moveSpeed = 5f;
+    [Header("Hareket Ayarlar�")]
+    public float hareketHizi = 5f; // Karakterin h�z�n� Inspector panelinden de�i�tirebilirsin
+
     private Rigidbody2D rb;
-    private Vector2 movement;
+    private Vector2 hareketYonu;
 
     void Start()
     {
+        // Karakterin �zerindeki Rigidbody2D bile�enini otomatik olarak bulup al�yoruz
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        // Oyuncunun klavye girdilerini al�yoruz (W, A, S, D veya Y�n Tu�lar�)
+        float hareketX = Input.GetAxisRaw("Horizontal"); // A ve D tu�lar� (Sol/Sa�)
+        float hareketY = Input.GetAxisRaw("Vertical");   // W ve S tu�lar� (Yukar�/A�a��)
 
-        if (movement.x > 0)
-        {
-            transform.localScale = new Vector3(1, 1, 1);
-        }
-        else if (movement.x < 0)
-        {
-            transform.localScale = new Vector3(-1, 1, 1);
-        }
+        // Girdileri bir y�n vekt�r�ne d�n��t�r�yoruz
+        // .normalized kodu, karakterin �apraz giderken (�rne�in W ve D'ye ayn� anda basarken) normalden daha h�zl� gitmesini engeller.
+        hareketYonu = new Vector2(hareketX, hareketY).normalized;
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+        // Karakterin fiziksel hareketini ger�ekle�tiriyoruz
+        // Hareket y�n�n� h�z�m�zla �arp�p Rigidbody'nin h�z�na (velocity) e�itliyoruz
+        rb.linearVelocity = new Vector2(hareketYonu.x * hareketHizi, hareketYonu.y * hareketHizi);
     }
 }
